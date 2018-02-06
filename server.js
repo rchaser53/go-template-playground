dcf39c9f05e5b6a797f6db8a1eb7a36d4876b4a2
rchaser53 <tayoshizawa29@gmail.com>
2018-02-06 23:58:31 +0900

    add comment and implement fo dependency injection

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-// scriptタグが取得するjs
+// 毎回ファイルを読み込ませないためにオンメモリで情報は保持しておく
+const template = fs.readFileSync('template.js', { encoding: 'utf8'})
+
+// scriptタグが取得するjsを作成し送り返す
 app.get('/di.js', (req, res) => {
   // ここでtemplateにDBとかの値を入れる
-  const template = fs.readFileSync('template.js', { encoding: 'utf8'})
   const sendJsFile = template.replace(/{{(\s|\S)*}}/, 234)
   res.send(sendJsFile)
 })
