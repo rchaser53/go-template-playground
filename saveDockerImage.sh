f5984d439679885f56a54b6c3448079fccf02975
rchaser53 <tayoshizawa29@gmail.com>
2018-02-07 18:37:29 +0900

    use docker save in parallel

--- saveDockerImage.sh
+++ saveDockerImage.sh
@@ -3,6 +3,10 @@ images=$(docker images -q)
 i=1
 
 for image in $images; do
-  docker save $image -o ./cache/$cache$i
+  docker save $image -o ./cache/$cache$i &
+  # echo docker image_$i saved 
   i=`expr $i + 1`
 done
+
+# i think it shouldn't use 'wait' for parallel
+# wait
